30a02a5b4a11b2103ec4912cf0a8972dce447ded
weide <4a339047@stust.edu.tw>
2022-12-16 13:06:50 +0800

    物件導向說明

--- index.php
+++ index.php
@@ -1,23 +1,60 @@
 <?php
-$cat=new Animal; //導入Animal的類別
-$dog=new Animal;
+// $cat=new Animal; //導入Animal的類別
+// $dog=new Animal;
 
-echo $cat->type; //物件導向 cat的值會等於type定義的常數
-echo "<br>";
-echo $dog->type;
-echo "<br>";
+// echo $cat->type; //物件導向 cat的值會等於type定義的常數
+// echo "<br>";
+// echo $dog->type;
+// echo "<br>";
 
-$cat->run();
-$cat->speed();
+// $cat->run();
+// $cat->speed();
+
+// $cat=new Animal('小花','黑白相間','臭狗');
+// echo $cat->getType();
+// echo $cat->getName();
+// echo $cat->getColor();
+// echo"<br>";
+
+// $pig=new Animal('豬頭','有夠粉','胖子');
+// echo $pig->getType();
+// echo $pig->getName();
+// echo $pig->getColor();
+
+$dog=new Dog('柯基','棕色');
+echo $dog->getType();
+echo $dog->getName();
+echo $dog->getColor();
+
+// $cat=new Dog('豬頭','有夠粉','胖子');
+// echo $cat->getType();
+// echo $cat->getName();
+// echo $cat->getColor();
 Class Animal{
-    public $type='animal'; //public的定義 可以給外部自由讀值
-    public $name='John';
+    protected $type='animal'; //public的定義 可以給外部自由讀值
+    protected $name='John';
     protected $hair_color="brown";//protect 被保護的外部不能存取
 
-    public function __constructor(){
+    public function __construct($name,$color,$type){
         //建構式內容
+        // $this->run();
+        $this->name=$name;
+        $this->hair_color=$color;
+        $this->type=$type;
     }
+public function getName(){
 
+    return $this->name;
+}
+
+public function getColor(){
+
+    return $this->hair_color;
+}
+public function getType(){
+
+    return $this->type;
+}
     public function run(){
         //公開行為內容
         echo "我會跑喔喔喔";
@@ -31,4 +68,20 @@ Class Animal{
     }
 
 }
+class Cat extends Animal{
+public function __construct($name,$color){
+    $this->name=$name;
+    $this->hair_color=$color;
+    $this->type='貓';
+
+}
+}
+class Dog extends Animal{
+    public function __construct($name,$color){//construct 前面是兩個底線
+        $this->name=$name;
+        $this->hair_color=$color;
+        $this->type='狗';
+    
+    }
+    }
 ?>
