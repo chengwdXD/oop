5f6704945b9dd5a6049ad1410491d7a42c002681
weide <4a339047@stust.edu.tw>
2022-12-16 10:39:13 +0800

    物件中方法和權限的關係

--- index.php
+++ index.php
@@ -1,12 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+$cat=new Animal; //導入Animal的類別
+$dog=new Animal;
+
+echo $cat->type; //物件導向 cat的值會等於type定義的常數
+echo "<br>";
+echo $dog->type;
+echo "<br>";
+
+$cat->run();
+$cat->speed();
+Class Animal{
+    public $type='animal'; //public的定義 可以給外部自由讀值
+    public $name='John';
+    protected $hair_color="brown";//protect 被保護的外部不能存取
+
+    public function __constructor(){
+        //建構式內容
+    }
+
+    public function run(){
+        //公開行為內容
+        echo "我會跑喔喔喔";
+        $this->speed(); //在class裡面的function互相echo 不受private和protect影響
+        echo $this->type; //echo public的function 如果內部有echo其他private和protect的一樣可以出來
+    }
+
+    private function speed(){
+        //私有行為內容
+        echo"我會叫喔喔喔喔喔";
+    }
+
+}
+?>
